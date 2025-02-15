1656ff703ddd93f4580285ae17289e7e6d3445c7
Ganesh Gowda <135863982+gowdaganesh005@users.noreply.github.com>
2025-02-15 05:15:15 +0000

    empty folder

--- app/scripts/setup-search.ts
+++
@@ -1,8 +0,0 @@
-import { setupQueryCapabilites } from "@/prisma/SearchUsers";
-
-async function runSetup(){
-    await setupQueryCapabilites()
-    console.log('search capabilites added')
-}
-
-runSetup().catch(console.error)
--- prisma/migrations/20250207024914_adding_some_changes/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "onboarded" BOOLEAN NOT NULL DEFAULT false,
-ALTER COLUMN "username" DROP NOT NULL;
